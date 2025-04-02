a2c6bc8bafe7ddca91779fdbefc431835534eb51
Suril Jignesh Purohit <45599204+SurilPurohit@users.noreply.github.com>
2025-04-02 13:25:18 -0400

    Update schedule_creator_job.rb

--- app/jobs/schedule_creator_job.rb
+++ app/jobs/schedule_creator_job.rb
@@ -2,7 +2,7 @@ class ScheduleCreatorJob < ApplicationJob
   queue_as :default
 
   def perform(schedule_data)
-    grok_service = GrokService.new(ENV['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'])
+    grok_service = GrokService.new(ENV[GROK_API_KEY])
     response = grok_api.create_schedule(schedule_data)
     # Handle response as needed
   end
